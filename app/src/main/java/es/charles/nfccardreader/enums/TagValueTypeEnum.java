0e963e1b90b0a4bc92b601ba16c2da37d7842c0a
Carlos del Campo <carlos.del.campo@outlook.es>
2023-08-19 22:20:23 +0200

    Initial commit

---
+++ app/src/main/java/es/charles/nfccardreader/enums/TagValueTypeEnum.java
@@ -0,0 +1,5 @@
+package es.charles.nfccardreader.enums;
+
+public enum TagValueTypeEnum {
+	BINARY, NUMERIC, TEXT, MIXED, DOL, TEMPLATE
+}
---
+++ app/src/main/java/es/charles/nfccardreader/parser/apdu/IFile.java
@@ -0,0 +1,22 @@
+package es.charles.nfccardreader.parser.apdu;
+
+import es.charles.nfccardreader.iso7816emv.TagAndLength;
+
+import java.util.List;
+
+/**
+ * Interface for File to parse
+ */
+public interface IFile {
+
+	/**
+	 * Method to parse byte data
+	 * 
+	 * @param pData
+	 *            byte to parse
+	 * @param pList
+	 *            Tag and length
+	 */
+	void parse(final byte[] pData, final List<TagAndLength> pList);
+
+}
